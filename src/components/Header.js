1e0893e6ad212d08584850a0918a6985283271f5
Amith <amith.r2901@gmail.com>
2024-03-14 10:45:06 +0530

    Initial Setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = ()=>{
+    return(
+        <div className="container">
+            <div className="title">
+            Plan A Trip
+            </div>
+        </div>
+    )
+}
+export default Header;
